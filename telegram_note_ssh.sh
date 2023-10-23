c57eff2d1c700253c75315ed4a12543d263b7ce2
Vasily Sankov <vsankov@gmail.com>
2023-10-23 11:53:38 +0700

    Create telegram_note_ssh.sh
    shall

---
+++ telegram_note_ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Telegram notification
+# Send msg when your server load to high
+token="_TOKEN_" # put your token here 
+chat_id="_CHAT_ID_" # your chat_id for sending notification
+sendmsg="https://api.telegram.org/bot$token/sendMessage?parse_mode=markdown" # url for sending msg
+sendfile="https://api.telegram.org/bot$token/sendDocument?parse_mode=markdown" # url for sending files
+date="$(date "+%d-%b-%Y-%H:%M")"
+caption_file=/tmp/ssh_caption_file.txt
+msg=/tmp/ssh_msg_info.txt
+curl http://ip-api.com/json/$PAM_RHOST -s -o $caption_file #take info about new connection
+country=$(cat $caption_file | jq '.country' | sed 's/"//g')
+city=$(cat $caption_file | jq '.city' | sed 's/"//g')
+org=$(cat $caption_file | jq '.as' | sed 's/"//g')
+echo -e "📡New SSH login\n*🤖$PAM_USER* logged in on 🖥*$HOSTNAME* at $date from IP: $PAM_RHOST\n-Country:*$country*\n-City=*$city*\n-Organisation=*$org*" > $msg
+curl $sendmsg -d chat_id=$chat_id -d text="$(<$msg)" 
+rm /tmp/ssh_caption_file.txt
+rm /tmp/ssh_msg_info.txt
